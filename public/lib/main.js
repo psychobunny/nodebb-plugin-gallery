93f33e1ae2a9db1ce9d0764d5f98122dde3bf36c
psychobunny <psycho.bunny@hotmail.com>
2015-04-08 14:22:43 -0400

    updated to work with 0.7

--- public/lib/main.js
+++ public/lib/main.js
@@ -3,7 +3,8 @@
 
 	jQuery('document').ready(function() {
 		$(window).on('action:ajaxify.end', function() {
-			$(".post-content a:has(img)").attr('rel', 'gallery').fancybox();
+			alert('test');
+			$('[component="post/content"] .img-markdown').attr('rel', 'gallery').fancybox();
 		});
 	});
 }());
