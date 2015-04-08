c9cd52871ea4de605f2b4927e5d73824a9ef19e8
psychobunny <psycho.bunny@hotmail.com>
2015-04-08 14:24:20 -0400

    woops, alert

--- public/lib/main.js
+++ public/lib/main.js
@@ -3,7 +3,6 @@
 
 	jQuery('document').ready(function() {
 		$(window).on('action:ajaxify.end', function() {
-			alert('test');
 			$('[component="post/content"] .img-markdown').attr('rel', 'gallery').fancybox();
 		});
 	});
